c8b35dad81ca736a8cc07c5b30b4118f712822bd
L1m3Kun <sungku2757@gmail.com>
2025-10-14 16:32:20 +0900

    ✨ Feat: store system added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,13 @@ import type { RouteProps } from "react-router-dom";
 import Landing from "@/pages/Landing";
 import Ranking from "@/pages/Ranking";
 import Status from "@/pages/Status";
+import Store from "@/pages/Store";
 
 export const ROUTE_CONFIG_MAIN: RouteProps[] = [
   { index: true, element: <Landing /> },
   { path: "status", element: <Status /> },
   { path: "ranking", element: <Ranking /> },
+  { path: "store", element: <Store /> },
   { path: "*", element: <Landing /> },
 ];
 
@@ -14,4 +16,5 @@ export const ROUTE_PATH = {
   ROOT: "/",
   STATUS: "/status",
   RANKING: "/ranking",
+  STORE: "/store",
 } as const;
